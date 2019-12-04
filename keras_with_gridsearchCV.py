9c74529a74c46b4231743502fac30285f553d600
Burak Ketmen <45431865+buraketmen@users.noreply.github.com>
2019-12-04 16:05:51 +0300

    keras with gridsearch

---
+++ keras_with_gridsearchCV.py
@@ -0,0 +1,42 @@
+
+import numpy
+import pandas as pd
+from sklearn.model_selection import GridSearchCV
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1" #Disable GPU
+# Function to create model, required for KerasClassifier
+
+def create_model():
+	# create model
+	model = Sequential()
+	model.add(Dense(12, input_dim=8, activation='relu'))
+	model.add(Dense(1, activation='sigmoid'))
+	# Compile model
+	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
+# fix random seed for reproducibility
+seed = 7
+numpy.random.seed(seed)
+# load dataset
+dataset = numpy.loadtxt("Data/diabetes.csv", delimiter=",",skiprows=1)
+# split into input (X) and output (Y) variables
+X = dataset[:,0:8]
+Y = dataset[:,8]
+# create model
+model = KerasClassifier(build_fn=create_model, verbose=2)
+# define the grid search parameters
+batch_size = [10, 20, 40, 60, 80, 100]
+epochs = [10, 50, 100]
+param_grid = dict(batch_size=batch_size, epochs=epochs)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1, cv=5)
+grid_result = grid.fit(X, Y)
+# summarize results
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
